7617e06c4daec5ff49ee0b0c8b398ca185bae1fe
Josh Israel <uno.braid@gmail.com>
2023-12-15 17:22:15 -0300

    fix(client): Fixed the style of the edit-table and create-table pages, along with Its components.

--- apps/client/src/components/DiscardChangesDialog.vue
+++ apps/client/src/components/DiscardChangesDialog.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-defineProps<{ show?: boolean }>()
 const emits = defineEmits<{ (_e: 'close'): void; (_e: 'confirm'): void }>()
 
 const cardRef = ref<HTMLElement>()
@@ -14,24 +13,22 @@ function confirm() {
 </script>
 
 <template>
-  <Transition name="fade">
-    <div v-if="show" class="modal justify-center">
-      <div ref="cardRef" class="card-primary gap-3 w-full lg:max-w-xl lg:mx-auto">
-        <h1 class="text-danger font-semibold">
-          {{ $t('components.discard-changes-dialog.title') }}
-        </h1>
-        <p class="text-sm">
-          {{ $t('components.discard-changes-dialog.message') }}
-        </p>
-        <div class="grid grid-cols-2 gap-3 mt-3">
-          <button class="btn btn-accent" @click.prevent="emits('close')">
-            {{ $t('components.discard-changes-dialog.close') }}
-          </button>
-          <button class="btn btn-primary" @click.prevent="confirm">
-            {{ $t('components.discard-changes-dialog.confirm') }}
-          </button>
-        </div>
+  <div class="modal justify-center">
+    <div ref="cardRef" class="card-secondary gap-2 lg:gap-3 w-full lg:max-w-xl lg:mx-auto">
+      <h1 class="text-danger font-semibold">
+        {{ $t('components.discard-changes-dialog.title') }}
+      </h1>
+      <p class="text-sm lg:text-base">
+        {{ $t('components.discard-changes-dialog.message') }}
+      </p>
+      <div class="grid grid-cols-2 gap-2 lg:gap-3 mt-2 lg:mt-3">
+        <button class="btn btn-primary" @click.prevent="emits('close')">
+          {{ $t('components.discard-changes-dialog.close') }}
+        </button>
+        <button class="btn btn-secondary" @click.prevent="confirm">
+          {{ $t('components.discard-changes-dialog.confirm') }}
+        </button>
       </div>
     </div>
-  </Transition>
+  </div>
 </template>
